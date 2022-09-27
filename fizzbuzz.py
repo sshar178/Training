7ded484d71a28b733e1b2da2099c20c4c8e1e5c5
sshar178 <shiv.a.sharma@capgemini.com>
2022-09-27 11:16:46 +0100

    fizzbuzz done added exercise

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,26 +1,27 @@
-modlist = [(3, "fizz"),(5,"buzz"),(7,"bang")]
-
-for x in range(1,1106):
+modlist = [(3, "fizz"),(5,"buzz"),(7,"bang")] ##list for the normal things 
+NUMBER = 0
+OUTPUT = 1
+for x in range(1,7736):
     out = ""
-    for num in modlist:
-        if x%num[0] == 0:
-            if x%17 == 0:
-                out = num[1] + out
+    for num in modlist: 
+        if x%num[NUMBER] == 0: #divisible by the number part of the list
+            if x%17 == 0: #other way around for 17
+                out = num[OUTPUT] + out  #add word part of the list to output
             else:
-                out += num[1]
-    if x%11 == 0:
+                out += num[OUTPUT]
+    if x%11 == 0:   #BINGBONG
         out = "bong"
     if x%13 == 0:
-        if x%11 == 0 or x%7 == 0 or x%5 == 0:
-            firstb = out.index("b")
-            if x%17 ==0:
-                out = out[firstb:] + "fezz" + out[:firstb]
+        if x%11 == 0 or x%7 == 0 or x%5 == 0:   #if div by 13 and any of the nums that give an output with a B in their word
+            firstb = out.index("b") #find first occurence of B
+            if x%17 ==0:    #might be wrong here
+                out = out[firstb:] + "fezz" + out[:firstb] #add after first b
             else:
-                out = out[:firstb] + "fezz" + out[firstb:]
+                out = out[:firstb] + "fezz" + out[firstb:]  #add before first b
         elif x%17 == 0: 
-            out = "fezz" + out
+            out = "fezz" + out  #reverse
         else:
-            out = out+ "fezz"
+            out = out + "fezz" #for div by 3 or div by 13 case
     if out == "":
         print(x)
     else: print(out)
